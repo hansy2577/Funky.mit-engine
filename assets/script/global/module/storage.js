e06f30f85723d158a168750b64db1771eac8b538
hansy2577B <hansyngampaka@gmail.com>
2026-06-27 18:58:01 +0200

    Add files via upload

---
+++ assets/script/global/module/storage.js
@@ -0,0 +1,10 @@
+// FMD : funky.mit data
+
+function FMD_getStorage(name) {
+  return localStorage.getItem(name);
+}
+
+function FMD_setStorage(name,value) {
+  localStorage.setItem(name,value);
+}
+
