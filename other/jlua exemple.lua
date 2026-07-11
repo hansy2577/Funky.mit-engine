cf512deffc84b526d6fad37ae113d064f19fbc2b
hansy2577B <hansyngampaka@gmail.com>
2026-07-11 12:16:49 +0200

    Update jlua exemple.lua

--- other/jlua exemple.lua
+++ other/jlua exemple.lua
@@ -5,3 +5,24 @@ local myText = "Hello word !"
 local text = doc.createElement("a")
 text.innerText = myText
 doc.body.appendChild(text)
+
+-- say loop time every 1 secondes ( 1000 milliseconds )
+
+local time = 0
+loop(function ()
+    print("it been the "+time+" loop")
+end,1000)
+
+-- print in the console when the game ended
+
+function TheGameEnded()
+  print("you finish the song yay !!!")
+end
+
+local myLoop = loop(function ()
+  if game.song.ended then
+      TheGameEnded()
+      clearInterval(myLoop)
+      return;
+  end
+end,0)
